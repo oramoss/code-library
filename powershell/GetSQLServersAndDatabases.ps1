a21449c2a53aceaa1b92b660c473065ce53820d4
oramoss <jeff.moss@oramoss.com>
2019-09-08 13:26:31 +0100

    Added GetSQLServersAndDatabases.ps1

---
+++ powershell/GetSQLServersAndDatabases.ps1
@@ -0,0 +1,11 @@
+# Gets a list of SQL Servers and for each one, lists out how many databases are on that server and their names
+# It does not include the master database.
+$sqlservers = Get-AzResource -ResourceType Microsoft.Sql/servers
+foreach ($sqlserver in $sqlservers)
+{
+    $sqlserver.Name
+    $databases = Get-AzResource -ResourceType Microsoft.Sql/servers/databases|Where-Object {$_.Name -notlike "*master"}|Where-Object {$_.Name -like $r.Name + "/*"}
+	"Database Count:" + $databases.Count
+	"Databases..."
+	">>>" + $databases.Name
+}
