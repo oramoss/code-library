ebf34af3f9f219bf55dd1364f4dde80e24006239
oramoss <jeff.moss@oramoss.com>
2019-09-16 08:25:50 +0100

    Added GetNetworkHierarchy.ps1

--- powershell/GetNetworkHierarchy.ps1
+++ powershell/GetNetworkHierarchy.ps1
@@ -2,19 +2,19 @@
 $vnets = Get-AzVirtualNetwork
 foreach ($vnet in $vnets)
   {
-      "VNET: " + $vnet.Name
-      "Subnet Count: " + $vnet.Subnets.count
-      foreach ($subnet in $vnet.Subnets)
-        {
-          "..Subnet: " + $subnet.Name
-          if ($Subnet.IpConfigurations.count -eq 0)
-                                    {"Subnet has no IpConfigurations"}
-                                  else
-                                    {
-            foreach ($IpConfiguration in $Subnet.IpConfigurations)
-                                      {
-               "...." + $IpConfiguration.Id.substring($IpConfiguration.Id.indexof("resourceGroups")+15)
-                                                  }
-                                                }
-        }
+    "VNET: " + $vnet.Name
+    "Subnet Count: " + $vnet.Subnets.count
+    foreach ($subnet in $vnet.Subnets)
+      {
+        "..Subnet: " + $subnet.Name
+        if ($Subnet.IpConfigurations.count -eq 0)
+          {"Subnet has no IpConfigurations"}
+        else
+          {
+          foreach ($IpConfiguration in $Subnet.IpConfigurations)
+            {
+	          "...." + $IpConfiguration.Id.substring($IpConfiguration.Id.indexof("resourceGroups")+15)
+	        }
+          }
+      }
   }
