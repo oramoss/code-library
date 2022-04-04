615b4b75a090a1def2f995126f3e34f8f785ce77
oramoss <jeff.moss@oramoss.com>
2022-04-04 10:11:11 +0100

    Updated script

--- powershell/AzurePowerShellMiscellaneous.ps1
+++ powershell/AzurePowerShellMiscellaneous.ps1
@@ -10,6 +10,12 @@ Get-AzVMImageSku -Location $Location -PublisherName "MicrosoftWindowsServer" -Of
 Get-AzVMImage -Location $Location -PublisherName "MicrosoftWindowsServer" -Offer "RHEL" -Skus "7.8"
 Get-AzVMImage -Location $Location -PublisherName $Publisher -Offer $offer -Skus $Skus
 
+Get-AzVMImagePublisher -Location $Location #check all the publishers available
+Get-AzVMImageOffer -Location $Location -PublisherName "RedHat" #look for offers for a publisher (Hortonworks)
+Get-AzVMImageSku -Location $Location -PublisherName "RedHat" -Offer "RHEL" #view SKUs for an offer (hortonworks-sandbox)
+Get-AzVMImage -Location $Location -PublisherName "RedHat" -Offer "RHEL" -Skus "8_5"
+Get-AzVMImage -Location $Location -PublisherName $Publisher -Offer $offer -Skus $Skus
+
 $PSVersionTable
 
 
